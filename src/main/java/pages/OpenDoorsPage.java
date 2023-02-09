f4ccd6f65bfe7c24609c5d426b29d8f1bda6bcf5
NobodyThe1 <117182134+NobodyThe1@users.noreply.github.com>
2023-02-09 10:29:10 +0300

    Delete OpenDoorsPage.java

--- src/main/java/pages/OpenDoorsPage.java
+++
@@ -1,9 +0,0 @@
-package pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class OpenDoorsPage extends AbsPage {
-    public OpenDoorsPage(WebDriver driver) {
-        super(driver, "/events/near/open_doors/");
-    }
-}
